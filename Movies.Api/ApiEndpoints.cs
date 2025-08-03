4aadba9fcde14fc54da9694c51d09d4d1da6c757
Sara <reyhansara97@gmail.com>
2025-08-03 22:41:28 +0530

    feat - added movie retrieval logic

---
+++ Movies.Api/ApiEndpoints.cs
@@ -0,0 +1,19 @@
+﻿namespace Movies.Api
+{
+    public static class ApiEndpoints
+    {
+        private const string ApiBase = "api";
+        public static class Movies
+        {
+            private const string Base = $"{ApiBase}/movies";
+
+            public const string Create = Base;
+
+            public const string GetById = $"{Base}/{{id:guid}}";
+
+            public const string GetAll = Base;
+
+        }
+
+    }
+}
---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+using System.Net;
+
+namespace Movies.Api.Controllers
+{
+  
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [HttpPost(ApiEndpoints.Movies.Create)]
+        public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+        {
+            var movie = request.MapToMovie();
+            await _movieRepository.CreateAsync(movie);
+            return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
+        }
+
+        [HttpGet(ApiEndpoints.Movies.GetById)]
+        public async Task<IActionResult> Get([FromRoute] Guid id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+            if(movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie.MapToResponse());
+
+        }
+
+        [HttpGet(ApiEndpoints.Movies.GetAll)]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var movies = await _movieRepository.GetAllAsync();
+
+            var moviesResponse = movies.MapToResponse();
+            return Ok(moviesResponse);
+        }
+    }
+    
+}
