717770ed757cfc8cade2010f9c81957f8426ed79
Sara <reyhansara97@gmail.com>
2025-08-03 17:39:19 +0530

    feat - added a repository for movies operations

---
+++ Movies.Application/Repositories/IMovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using Movies.Application.Models;
+using System;
+
+namespace Movies.Application.Repositories
+{
+    public interface IMovieRepository
+    {
+       Task<bool> CreateAsync(Movie movie);
+       Task<Movie?> GetByIdAsync(Guid Id);
+       Task<IEnumerable<Movie>> GetAllAsync();
+
+       Task<bool> UpdateAsync(Movie movie);
+
+       Task<bool> DeleteByIdAsync(Guid Id);
+    }
+}
--- Movies.Contract/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Movies.Contracts
-{
-    public class Class1
-    {
-
-    }
-}
